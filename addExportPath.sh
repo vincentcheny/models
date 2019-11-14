0dd9a39c65df54806bde9852fc3980378df790e4
vincent <cy0906@163.com>
2019-11-15 02:16:46 +0800

    udpate export path

--- addExportPath.sh
+++ addExportPath.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=/usr/local/lib
-export PYTHONPATH=/home/user/models
-export GLUE_DIR=/home/user/glue_data
-export BERT_BASE_DIR=/home/user/uncased_L-12_H-768_A-12
-export OUTPUT_DIR=/home/user/datasets
+export PYTHONPATH=/home/user/tf-training/models
+export GLUE_DIR=/home/user/tf-training/glue_data
+export BERT_BASE_DIR=/home/user/tf-training/uncased_L-12_H-768_A-12
+export OUTPUT_DIR=/home/user/tf-training/datasets
 export TASK_NAME=MRPC
-export DATASETS_DIR=/home/user/datasets
-export MODEL_DIR=/home/user/results
+export DATASETS_DIR=/home/user/tf-training/datasets
+export MODEL_DIR=/home/user/tf-training/results
 export TASK=MRPC
