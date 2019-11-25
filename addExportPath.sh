34d38979ac7ac3bce7b151d1db2fee3c233df62f
vincent <cy0906@163.com>
2019-11-25 18:40:33 +0800

    add multiworker config

--- addExportPath.sh
+++ addExportPath.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 if [ "$1" == "local" ]
 then
-    export PYTHONPATH=/mnt/d/GitRepository/BERT/models
-    export GLUE_DIR=/mnt/d/GitRepository/BERT/glue_data
-    export BERT_BASE_DIR=/mnt/d/GitRepository/BERT/uncased_L-12_H-768_A-12
-    export OUTPUT_DIR=/mnt/d/GitRepository/BERT/datasets
+    export PYTHONPATH=/mnt/d/GitRepository/Imagenet/models
+    export GLUE_DIR=/mnt/d/GitRepository/Imagenet/glue_data
+    export BERT_BASE_DIR=/mnt/d/GitRepository/Imagenet/uncased_L-12_H-768_A-12
+    export OUTPUT_DIR=/mnt/d/GitRepository/Imagenet/datasets
     export TASK_NAME=MRPC
-    export DATASETS_DIR=/mnt/d/GitRepository/BERT/datasets
-    export MODEL_DIR=/mnt/d/GitRepository/BERT/results
+    export DATASETS_DIR=/mnt/d/GitRepository/Imagenet/datasets
+    export MODEL_DIR=/mnt/d/GitRepository/Imagenet/results
     export TASK=MRPC
 elif [ "$1" == "server" ]
 then
